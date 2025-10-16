eae76bd73562130a8b5fd2c5955f5e677f861e47
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2025-10-16 16:00:24 +0700

    Halaman admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UmkmController;
+use App\Http\Controllers\DashboardController;
 
 
 // Routes untuk Guest UMKM
@@ -12,3 +13,6 @@ Route::get('/umkm/detail/{id}', [UmkmController::class, 'show']);
 // Routes untuk Auth
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
+// Route untuk admin
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
