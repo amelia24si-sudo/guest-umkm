15992c37b8235d435bae7b31ef3cf79c3492e1ad
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2025-10-27 19:36:16 +0700

    partial view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,26 +9,17 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $users = User::all();
         return view('users.index', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('users.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -55,25 +46,16 @@ class UserController extends Controller
             ->with('success', 'User berhasil dibuat!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(User $user)
     {
         return view('users.edit', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, User $user)
     {
         $validator = Validator::make($request->all(), [
@@ -105,9 +87,6 @@ class UserController extends Controller
             ->with('success', 'User berhasil diupdate!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
         $user->delete();
