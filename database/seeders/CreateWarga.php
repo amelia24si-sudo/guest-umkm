2e074b10644c5b1705455dd8fefa68d8d19fabec
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2025-11-15 11:18:00 +0700

    seeder

--- database/seeders/CreateWarga.php
+++ database/seeders/CreateWarga.php
@@ -1,9 +1,9 @@
 <?php
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory;
 
 class CreateWarga extends Seeder
 {
@@ -16,16 +16,18 @@ class CreateWarga extends Seeder
 
         foreach (range(1, 10) as $index) {
             DB::table('warga')->insert([
-                'no_ktp'        => $faker->nik(), // Generate NIK Indonesia
-                'nama'          => $faker->name(), // Generate nama lengkap
+                'no_ktp'        => $faker->nik(),
+                'nama'          => $faker->name(),
                 'jenis_kelamin' => $faker->randomElement(['L', 'P']),
-                'agama'         => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']),
+                'agama'         => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha']),
                 'pekerjaan'     => $faker->jobTitle(),
-                'telp'          => $faker->phoneNumber(),
+                'telp'          => '08' . $faker->numerify('##########'),
                 'email'         => $faker->unique()->safeEmail(),
                 'alamat'        => $faker->address(),
                 'rt'            => $faker->numberBetween(1, 10),
                 'rw'            => $faker->numberBetween(1, 5),
+                'created_at'    => now(),
+                'updated_at'    => now(),
             ]);
         }
     }
